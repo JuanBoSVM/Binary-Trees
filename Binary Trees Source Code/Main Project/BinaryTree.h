3c0eb8862b98e7def85e64d6ad815c38391a1e1c
Juan Bosco Sánchez Vargas Mora <JuanBoSVM@gmail.com>
2021-04-04 23:02:04 -0500

    Added a search method and finished the rebalancing functions

--- Binary Trees Source Code/Main Project/BinaryTree.h
+++ Binary Trees Source Code/Main Project/BinaryTree.h
@@ -17,13 +17,17 @@ enum class UB_TYPE {
 
 template <typename T>
 
-class Tree {
+class BinaryTree {
 
 private:
 
 	Node<T>
 		* root		{ nullptr },
 		* unbalanced{ nullptr };
+	int
+		nodes { 0 },
+		length{ 0 };
+
 
 /************************************************************************/
 /* 								DECLARATIONS	                        */
@@ -33,6 +37,9 @@ public:
 
 	// Return the root as a constant
 	const Node<T> getRoot() const;
+
+	// Does the tree contain a specific node?
+	bool contains(T);
 
 	// Add a new node to the tree
 	void addNode(T);
@@ -76,7 +83,7 @@ private:
 
 public:
 
-	~Tree();
+	~BinaryTree();
 };
 
 
@@ -85,10 +92,33 @@ public:
 /************************************************************************/
 
 template <typename T>
-const Node<T> Tree<T>::getRoot() const { return *root; }
+const Node<T> BinaryTree<T>::getRoot() const { return *root; }
 
 template <typename T>
-void Tree<T>::addNode(T _value) {
+bool BinaryTree<T>::contains(T _data) {
+	
+	Node<T>* cNode{ root };
+
+	// Main search loop
+	while (cNode != nullptr) {
+
+		// The node was found
+		if (cNode->value == _data) { return true; }
+
+		// If the data looked for is lower than the current node,
+		// advance to the left side of the tree
+		else if (_data < cNode->value) { cNode = cNode->lChild; }
+
+		// Otherwise, advance to the right side
+		else { cNode = cNode->rChild; }
+	}
+
+	// The node wasn't found
+	return false;
+}
+
+template <typename T>
+void BinaryTree<T>::addNode(T _value) {
 
 	// Save it as root if there's none
 	if (root == nullptr) {
@@ -166,10 +196,12 @@ void Tree<T>::addNode(T _value) {
 			}
 		}
 	}
+
+	nodes++;
 }
 
 template <typename T>
-UB_TYPE Tree<T>::getUbType(Node<T>* _ubNode) {
+UB_TYPE BinaryTree<T>::getUbType(Node<T>* _ubNode) {
 
 	// The first step is left
 	if (_ubNode->lWeight > _ubNode->rWeight) {
@@ -199,7 +231,7 @@ UB_TYPE Tree<T>::getUbType(Node<T>* _ubNode) {
 }
 
 template <typename T>
-void Tree<T>::rebalance() {
+void BinaryTree<T>::rebalance() {
 	
 	UB_TYPE cUnbalance{ getUbType(unbalanced) }; // Current unbalance
 
@@ -227,13 +259,20 @@ void Tree<T>::rebalance() {
 }
 
 template <typename T>
-void Tree<T>::update() { update(root); }
+void BinaryTree<T>::update() {
+	
+	// Update each node, starting from the root
+	update(root);
+
+	// Update the length of the tree
+	length = root->mLength;
+}
 
 template <typename T>
-void Tree<T>::update(Node<T>* _cNode) {
+void BinaryTree<T>::update(Node<T>* _cNode) {
 
 	// If the tree's just the root, ignore it
-	if (root->lChild != nullptr && root->rChild != nullptr) {
+	if (!root->isLeaf) {
 
 		// Look for the leafs from the left side
 		if (_cNode->lChild != nullptr) { update(_cNode->lChild); }
@@ -251,7 +290,7 @@ void Tree<T>::update(Node<T>* _cNode) {
 }
 
 template <typename T>
-void Tree<T>::pullRight(Node<T>* _head) {
+void BinaryTree<T>::pullRight(Node<T>* _head) {
 
 	// Check if we have to move the root
 	if (_head == root) {
@@ -299,12 +338,39 @@ void Tree<T>::pullRight(Node<T>* _head) {
 	// The root of the tree won't change
 	else {
 
+		// List of modified nodes
+		Node<T>
+			* nodeA{ _head },
+			* nodeB{ _head->parent },
+			* nodeC{ _head->lChild },
+			* nodeD{ _head->lChild->rChild };
+
+		// Change node B's right child to node C
+		// and adjust node C's parent
+		nodeB->rChild = nodeC;
+		nodeC->parent = nodeB;
 		
+		// Change node C's right child to node A
+		// and adjust node A's parent to node C
+		nodeC->rChild = nodeA;
+		nodeA->parent = nodeC;
+
+		// Change node A's left child to node D
+		// and adjust node D's parent as long as it isn't empty
+		nodeA->lChild = nodeD;
+		if (nodeD != nullptr) { nodeD->parent = nodeA; }
+
+		// Update the information
+		// of the modified nodes
+		nodeA->update();
+		nodeB->update();
+		nodeC->update();
+		nodeD->update();
 	}
 }
 
 template <typename T>
-void Tree<T>::pullLeft(Node<T>* _head) {
+void BinaryTree<T>::pullLeft(Node<T>* _head) {
 
 	// Check if we have to move the root
 	if (_head == root) {
@@ -352,12 +418,39 @@ void Tree<T>::pullLeft(Node<T>* _head) {
 	// The root of the tree won't change
 	else {
 
+		// List of modified nodes
+		Node<T>
+			* nodeA{ _head },
+			* nodeB{ _head->parent },
+			* nodeC{ _head->rChild },
+			* nodeD{ _head->rChild->lChild };
 
+		// Change node B's right child to node C
+		// and adjust node C's parent
+		nodeB->lChild = nodeC;
+		nodeC->parent = nodeB;
+
+		// Change node C's right child to node A
+		// and adjust node A's parent to node C
+		nodeC->lChild = nodeA;
+		nodeA->parent = nodeC;
+
+		// Change node A's left child to node D
+		// and adjust node D's parent as long as it isn't empty
+		nodeA->rChild = nodeD;
+		if (nodeD != nullptr) { nodeD->parent = nodeA; }
+
+		// Update the information
+		// of the modified nodes
+		nodeA->update();
+		nodeB->update();
+		nodeC->update();
+		nodeD->update();
 	}
 }
 
 template <typename T>
-void Tree<T>::delNodes(Node<T>* _node) {
+void BinaryTree<T>::delNodes(Node<T>* _node) {
 
 	// If there are nodes to the left, delete them
 	if (_node->lChild != nullptr) { delNodes(_node->lChild); }
@@ -370,7 +463,7 @@ void Tree<T>::delNodes(Node<T>* _node) {
 }
 
 template <typename T>
-bool Tree<T>::isBalanced(Node<T>* _node) {
+bool BinaryTree<T>::isBalanced(Node<T>* _node) {
 
 	// If the node has no children, return true
 	if (_node->isLeaf) {
@@ -399,12 +492,12 @@ bool Tree<T>::isBalanced(Node<T>* _node) {
 }
 
 template <typename T>
-void Tree<T>::updateWeight(Node<T>* _node) {
+void BinaryTree<T>::updateWeight(Node<T>* _node) {
 	
 	// If the node's a leaf, update it's info
 	if (_node->isLeaf) { _node->update(); }
 
-	// Look for the root parent from parent
+	// Look for the root, parent from parent
 	while (_node->parent != nullptr) {
 
 		// If the node's the left child,
@@ -427,7 +520,7 @@ void Tree<T>::updateWeight(Node<T>* _node) {
 }
 
 template <typename T>
-Tree<T>::~Tree() { delNodes(root); }
+BinaryTree<T>::~BinaryTree() { delNodes(root); }
 
 
 /************************************************************************/
@@ -435,4 +528,4 @@ Tree<T>::~Tree() { delNodes(root); }
 /************************************************************************/
 
 template <typename T>
-Tree<T>::operator bool() { return isBalanced(root); }
+BinaryTree<T>::operator bool() { return isBalanced(root); }
--- Binary Trees Source Code/Main Project/Source.cpp
+++ Binary Trees Source Code/Main Project/Source.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 
-#include "Tree.h"
+#include "BinaryTree.h"
 
 
 /************************************************************************/
@@ -9,27 +9,28 @@
 /************************************************************************/
 
 int main() {
-	Tree<int> mainTree;
+	BinaryTree<int> Tree;
 	std::vector<int> input{ 10, 20, 15, 25, 30, 16, 18, 19 };
 
 	for (int i : input) {
 		
 		// Add the new input to the tree
-		mainTree.addNode(i);
+		// only if it doesn't exist already
+		if (!Tree.contains(i)) { Tree.addNode(i); }
 		
 		// Check if the tree's unbalanced
-		if (!mainTree) {
+		if (!Tree) {
 			
 			// Print the before version
 			
 
 			// Rebalance the tree
-			mainTree.rebalance();
-
-			// Update the tree's info
-			mainTree.update();
+			Tree.rebalance();
 		}
 
+		// Update the tree's info
+		Tree.update();
+		
 		//Print the result
 	}
 }
